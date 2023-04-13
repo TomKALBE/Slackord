1659ed15d722682894264f3752e9653399fb7019
jiedevm <dieudonne.allognon@yahoo.com>
2023-04-13 11:58:56 +0200

    feat: setup client ws services

--- front/src/services/WebsocketService.ts
+++ front/src/services/WebsocketService.ts
@@ -1,38 +1,38 @@
-import { Socket, io } from "socket.io-client";
+import { Socket, io } from 'socket.io-client'
 
 export type Message = {
-  content: String;
-  upvote: Number | null;
-  type: "PRIVATE" | "PUBLIC" | "CHANNEL";
-  created_at: Number;
-  updated_at: Number;
-  deleted_at: Number | null;
-  user_id: Number;
-  receiver_id: Number;
-};
+  content: String
+  upvote: Number | null
+  type: 'PRIVATE' | 'PUBLIC' | 'CHANNEL'
+  created_at: Number
+  updated_at: Number
+  deleted_at: Number | null
+  user_id: Number
+  receiver_id: Number
+}
 
 interface ServerToClientEvents {}
 
 interface ClientToServerEvents {
-  "client.new-message": (data: Message) => void;
+  'client.new-message': (data: Message) => void
 }
 
-type ClientSocket = Socket<ServerToClientEvents, ClientToServerEvents>;
+type ClientSocket = Socket<ServerToClientEvents, ClientToServerEvents>
 
-const clientSocket: ClientSocket = io(import.meta.env.VITE_SOCKET_URL);
+const clientSocket: ClientSocket = io(import.meta.env.VITE_SOCKET_URL)
 
-clientSocket.on("connect", () => {
+clientSocket.on('connect', () => {
   // clientSocket.emit("ping", "ô monde");
-});
+})
 
-clientSocket.on("disconnect", () => {
-  console.log(clientSocket.connected); // false
-});
+clientSocket.on('disconnect', () => {
+  console.log(clientSocket.connected) // false
+})
 
 export const SocketService = {
   sendMessage: (clientSocket: ClientSocket, data: Message) => {
-    clientSocket.emit("client.new-message", data);
+    clientSocket.emit('client.new-message', data)
   },
-};
+}
 
-export default clientSocket;
+export default clientSocket
