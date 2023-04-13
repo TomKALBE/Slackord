a2f089a4e425d19b87a57860d986f9147837178d
jiedevm <dieudonne.allognon@yahoo.com>
2023-04-13 14:42:52 +0200

    feat: client new message

--- websocket/src/main.ts
+++ websocket/src/main.ts
@@ -1,21 +1,27 @@
 import { createServer } from "http";
 import { Server } from "socket.io";
 import Env from "./utils/env";
-import { MessageResource } from "./data/resources";
+import { MessageResource, ChannelResource } from "./data/resources";
 
 interface ServerToClientEvents {
-
+    "server.new-message": (data: MessageResource) => void;
 }
 
 interface ClientToServerEvents {
     "client.new-message": (data: MessageResource) => void;
+    ping: (data: number) => void;
 }
 
 interface InterServerEvents {
     ping: () => void;
 }
 
-interface SocketData { }
+interface SocketData {
+    userId: number,
+    channelsId: Map<number, string>
+}
+
+const socketsMap = new Map<number, string>();
 
 const httpServer = createServer();
 
@@ -25,12 +31,58 @@ const io = new Server<ClientToServerEvents, ServerToClientEvents, InterServerEve
     },
 }).on("connection", async (socket) => {
     console.log("new connection");
+
+    socket.data.channelsId = new Map();
+
+    socket.on("ping", async (userId: number) => {
+        socketsMap.set(userId, socket.id);
+    });
+
     socket.on("client.new-message", async (data) => {
-        console.log('new message received')
+        fetch(`${Env.API_URL}/messages`,
+            {
+                method: "POST",
+                headers: { "content-type": "application/json" },
+                body: JSON.stringify(data)
+
+            }).then(async (response) => {
+                if (response.ok) {
+
+                    if (data.type === 'PRIVATE') {
+                        const receiverSocketId = socketsMap.get(data.receiver_id);
+
+                        if (receiverSocketId)
+                            io.to(receiverSocketId)
+                                .emit('server.new-message', data)
+                    }
+
+                    if (["CHANNEL", 'PUBLIC'].includes(data.type)) {
+                        if (!socket.data.channelsId?.has(data.receiver_id)) {
+                            const res = await fetch(`${Env.API_URL}/channels/${data.receiver_id}`, {
+                                method: "GET"
+                            });
+
+                            const channel = await res.json() as ChannelResource;
+
+                            socket.data.channelsId?.set(data.receiver_id, channel.name)
+
+                            socket.join(channel.name);
+                        }
+
+                        const channelRoom = socket.data.channelsId?.get(data.user_id)
+
+                        if (channelRoom) {
+                            socket.to(channelRoom).emit('server.new-message', data);
+                        }
+                    }
+                }
+
+            }).catch((error) => {
+                console.error(error);
+            })
     });
 });
 
-httpServer.listen({port: Number(Env.PORT)}, () => {
+httpServer.listen({ port: Number(Env.PORT) }, () => {
     console.log('Server running on port', Env.PORT, '...')
 });
-
