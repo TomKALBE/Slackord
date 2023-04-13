308ecb19259771d606e40692faf33aaf3ade8298
jiedevm <dieudonne.allognon@yahoo.com>
2023-04-13 11:58:56 +0200

    feat: setup client ws services

--- public/js/src/main.ts
+++ public/js/src/main.ts
@@ -1,9 +1,12 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-
-import './assets/main.css'
+import io from "@/services/WebsocketService";
+import { createApp } from "vue";
 import { FontAwesomeIcon } from "@/tools/fontawesome";
 
-const app = createApp(App)
-app.component("font-awesome-icon", FontAwesomeIcon)
-app.mount('#app')
+import "./assets/main.css";
+
+import App from "./App.vue";
+const app = createApp(App);
+
+app.provide("$socket", io);
+app.component("font-awesome-icon", FontAwesomeIcon);
+app.mount("#app");
---
+++ public/js/src/services/WebsocketService.ts
@@ -0,0 +1,38 @@
+import { Socket, io } from 'socket.io-client'
+
+export type Message = {
+  content: String
+  upvote: Number | null
+  type: 'PRIVATE' | 'PUBLIC' | 'CHANNEL'
+  created_at: Number
+  updated_at: Number
+  deleted_at: Number | null
+  user_id: Number
+  receiver_id: Number
+}
+
+interface ServerToClientEvents {}
+
+interface ClientToServerEvents {
+  'client.new-message': (data: Message) => void
+}
+
+type ClientSocket = Socket<ServerToClientEvents, ClientToServerEvents>
+
+const clientSocket: ClientSocket = io(import.meta.env.VITE_SOCKET_URL)
+
+clientSocket.on('connect', () => {
+  // clientSocket.emit("ping", "ô monde");
+})
+
+clientSocket.on('disconnect', () => {
+  console.log(clientSocket.connected) // false
+})
+
+export const SocketService = {
+  sendMessage: (clientSocket: ClientSocket, data: Message) => {
+    clientSocket.emit('client.new-message', data)
+  },
+}
+
+export default clientSocket
