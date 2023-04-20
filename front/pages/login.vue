c37ef4eb299f66279efa890f8e9ad797c5156e7f
Tom <tomkalbe@hotmail.fr>
2023-04-20 18:30:01 +0200

    fix: api client cors(proxy)

--- front/pages/login.vue
+++ front/pages/login.vue
@@ -3,37 +3,35 @@ const props = defineProps({
   maFonction: Function,
 })
 const handleClick = () => {
-    const res = useAuth().login()
-    if(res === true)
-        navigateTo('/')
+  const res = useAuth().login()
+  if (res === true)
+    navigateTo('/')
 }
 const login = async () => {
   try {
-      const res = await fetch('https://local.zombocom.app/api/register', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-          "email": "xxxtz",
-          "password": "xxx",
-          "pseudo": "xxxtz"
-        }),
+    const res = await fetch('/api/register', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        "email": "xxxtz",
+        "password": "xxx",
+        "pseudo": "xxxtz"
+      }),
     })
     const json = await res.json()
     console.log(json)
-   }catch(e) {
-      console.log(e)
-    }
+  } catch (e) {
+    console.log(e)
+  }
 }
-onMounted(()=>{
-    
+onMounted(() => {
+
 })
 </script>
 <template>
-  <div
-    class="main h-screen w-screen flex justify-center items-center bg-slate-650"
-  >
+  <div class="main h-screen w-screen flex justify-center items-center bg-slate-650">
     <div class="border-2 border-slate-450 rounded-md bg-slate-750 p-8">
       <div class="w-100 flex flex-col items-center">
         <h1 class="text-indigo-500 text-xl font-bold">Connexion</h1>
@@ -44,35 +42,19 @@ onMounted(()=>{
       </div>
       <form class="h-100 flex flex-col items-center justify-center mt-6">
         <div class="mb-6">
-          <label for="email" class="block mb-2 text-sm font-medium text-white"
-            >Email</label
-          >
-          <input
-            type="email"
-            id="email"
+          <label for="email" class="block mb-2 text-sm font-medium text-white">Email</label>
+          <input type="email" id="email"
             class="bg-gray-50 border border-gray-300 text-sm rounded-lg focus:ring-indigo-500 focus:border-indigo-500 block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white"
-            placeholder="name@zombo.com"
-            required
-          />
+            placeholder="name@zombo.com" required />
         </div>
         <div class="mb-6">
-          <label
-            for="password"
-            class="block mb-2 text-sm font-medium text-white"
-            >Mot de passe</label
-          >
-          <input
-            type="password"
-            id="password"
+          <label for="password" class="block mb-2 text-sm font-medium text-white">Mot de passe</label>
+          <input type="password" id="password"
             class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-indigo-500 focus:border-indigo-500 block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white"
-            required
-          />
+            required />
         </div>
-        <button
-          @click="handleClick"
-          type="button"
-          class="text-white bg-indigo-600 active:bg-indigo-700 duration-300 hover:scale-105 tran focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center"
-        >
+        <button @click="handleClick" type="button"
+          class="text-white bg-indigo-600 active:bg-indigo-700 duration-300 hover:scale-105 tran focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center">
           Connexion
         </button>
       </form>
@@ -83,17 +65,13 @@ onMounted(()=>{
 <style scoped>
 .main {
   background: rgb(20, 30, 45);
-  background: -webkit-linear-gradient(
-    to top,
-    rgb(61, 74, 91),
-    rgb(41, 54, 71),
-    rgb(20, 30, 45)
-  );
-  background: linear-gradient(
-    to bottom,
-    rgb(61, 74, 91),
-    rgb(41, 54, 71),
-    rgb(20, 30, 45)
-  );
+  background: -webkit-linear-gradient(to top,
+      rgb(61, 74, 91),
+      rgb(41, 54, 71),
+      rgb(20, 30, 45));
+  background: linear-gradient(to bottom,
+      rgb(61, 74, 91),
+      rgb(41, 54, 71),
+      rgb(20, 30, 45));
 }
 </style>
