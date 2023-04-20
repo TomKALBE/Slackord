d8be2bd56934fec01a0c4ab94c846e604be6f40a
Tom <tomkalbe@hotmail.fr>
2023-04-20 18:30:01 +0200

    fix: api client cors(proxy)

--- front/nuxt.config.ts
+++ front/nuxt.config.ts
@@ -11,7 +11,7 @@ export default defineNuxtConfig({
         viewer: true,
     },
     modules: [
-        '@nuxtjs/tailwindcss'
+        '@nuxtjs/tailwindcss',
     ],
     build: {
         transpile: [
@@ -28,18 +28,18 @@ export default defineNuxtConfig({
         '@fortawesome/fontawesome-svg-core/styles.css',
     ],
 
-    // vite: {
-    //     server: {
-    //         proxy: {
-    //             "^/api/.*": {
-    //                 target: "https://local.zombocom.app/api/register",
-    //                 changeOrigin: true,
-    //                 secure: false,
-    //                 rewrite: (path) => path.replace(/^\/api/, ""),
-    //             },
-    //         },
-    //     },
-    // },
+    vite: {
+        server: {
+            proxy: {
+                "^/api/.*": {
+                    target: process.env.API_URL,
+                    changeOrigin: true,
+                    secure: false,
+                    rewrite: (path) => path.replace(/^\/api/, ""),
+                },
+            },
+        },
+    },
 
     runtimeConfig: {
         apiSecret: '', // can be overridden by NUXT_API_SECRET environment variable
