5b957a18ca24fe7e1dcc524074fe0bfc0d5c1774
ALLOGNON Dieudonné <dieudonne.allognon@yahoo.com>
2023-05-22 16:26:57 +0200

    chore/nuxt migration (#17)
    * fix multipledropdown
    
    * Animation notification
    
    * login
    
    * feat: Setup typescript with server socket
    
    * feat: setup server ws services
    
    * feat: setup client ws services
    
    * chore: app folders architecture reorganization
    
    * chore: migration vue -> nuxt
    
    * fix: api client cors(proxy)
    
    * fix: rebase error
    
    * migrating vue to nuxt
    
    ---------
    
    Co-authored-by: Tom <tomkalbe@hotmail.fr>
    Co-authored-by: Dieudonne <dieudonne.allognon@yahoo.com>

---
+++ front/app.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
+  </div>
+</template>
+<style>
+/* .fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s;
+}
+.fade-enter,
+.fade-leave-active {
+  opacity: 0;
+} */
+</style>
+<script setup>
+import getClientSocket from './utils/WebSocketService';
+
+const nuxtApp = useNuxtApp()
+nuxtApp.provide('socket', getClientSocket(useRuntimeConfig().public.socketUrl))
+
+</script>
