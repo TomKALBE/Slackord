f9237fdc14445d43481afe10ef6ec88e984d83ab
jiedevm <dieudonne.allognon@yahoo.com>
2023-06-28 19:13:24 +0200

    feat: channel new message (websocket)

--- websocket/src/main.ts
+++ websocket/src/main.ts
@@ -156,40 +156,37 @@ const io = new Server<ClientToServerEvents, ServerToClientEvents, InterServerEve
                 body: JSON.stringify(data)
 
             }).then(async (response) => {
-                if (response.ok) {
-                    if (data.type === 'PRIVATE') {
-                        const receiverSocketId = socketsMap.get(data.receiver_id);
+                if (!response.ok)
+                    return;
 
-                        if (receiverSocketId)
-                            io.to(receiverSocketId)
-                                .emit('server.new-message', data)
-                    }
-
-                    if (["CHANNEL", 'PUBLIC'].includes(data.type)) {
-                        if (!socket.data.channelsId?.has(data.receiver_id)) {
-                            const res = await fetch(`${Env.API_URL}/channels/${data.receiver_id}`, {
-                                method: "GET"
-                            });
-
-                            const channel = await res.json() as ChannelResource;
-
-                            socket.data.channelsId?.set(data.receiver_id, channel.name)
-
-                            socket.join(channel.name);
-                        }
-
-                        const channelRoom = socket.data.channelsId?.get(data.user_id)
-
-                        if (channelRoom) {
-                            socket.to(channelRoom).emit('server.new-message', data);
-                        }
-                    }
-                }
-
-                if (callback) {
+                if (callback)
                     callback({ ok: true });
+
+                if (data.type === 'PRIVATE') {
+                    const receiverSocketId = socketsMap.get(data.receiver_id);
+
+                    if (receiverSocketId)
+                        io.to(receiverSocketId)
+                            .emit('server.new-message', data)
                 }
 
+                if (["CHANNEL", 'PUBLIC'].includes(data.type)) {
+
+                    const members = await (fetch(`${Env.API_URL}/servers/${data.receiver_id}?_expand=members`).then(res => res.json()));
+
+                    members.forEach((member: any) => {
+
+                        if (!socketsMap.has(member.userId))
+                            return;
+
+                        const receiverSocketId = socketsMap.get(member.userId);
+
+                        if (receiverSocketId) {
+                            io.to(receiverSocketId)
+                                .emit('server.new-message', data);
+                        }
+                    });
+                }
             }).catch((error) => {
                 console.error(error);
 
